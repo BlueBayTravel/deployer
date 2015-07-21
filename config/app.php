78c9c84978257cd2ced799c44130735ac073be04
James Brooks <jbrooksuk@me.com>
2015-07-21 13:48:05 +0100

    Update Cipher

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'key' => env('APP_KEY', 'cHKwVTJCF75DKo9bta72ZNrkS6Q0tqYy'),
 
-    'cipher' => MCRYPT_RIJNDAEL_128,
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
